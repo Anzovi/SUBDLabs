0dc1699395b16d51584e552126a3d7ff7ed1eb8b
unknown <anckproff@gmail.com>
2022-10-16 15:26:45 +0300

    Starting point

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', 'MainController@main');
+Route::get('/ShowUnitedTable', 'MainController@ShowUnitedTable','ShowUnitedTable');
+Route::post('/ChangeData','MainController@ChangeData','ChangeData');
+Route::post('/DeleteData','MainController@DeleteData','DeleteData');
+Route::post('/AddData','MainController@AddData','AddData');
